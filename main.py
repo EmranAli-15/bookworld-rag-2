bfe79a6bee0c1e628b7ad84fa13837d605286fb0
EmranAli-15 <mohammademranali15@gmail.com>
2026-09-04 22:08:51 +0600

    setup

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import os
+from dotenv import load_dotenv
 from fastapi import FastAPI
 app = FastAPI()
+
+load_dotenv()
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+db_url = os.getenv("DB_URL")
 
 @app.get("/")
 def read_root():
